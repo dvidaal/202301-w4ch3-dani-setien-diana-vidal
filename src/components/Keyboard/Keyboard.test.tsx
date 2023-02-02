40831363c8c6b97a5c76f89d1cb9313d6eb47280
Diana Vidal <dvidaal@gmail.com>
2023-02-02 23:50:30 +0100

    Add tests for keyboard accessibility

--- src/components/Keyboard/Keyboard.test.tsx
+++ src/components/Keyboard/Keyboard.test.tsx
@@ -1,14 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Keyboard from "./Keyboard";
 
 describe("Given a component Keybord", () => {
   describe("When rendered", () => {
-    test("Then it should show all the numbers in the keyboard", () => {
+    test("Then it should show a button with a number on it", () => {
       render(<Keyboard />);
 
-      const allKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+      const allKeys = screen.getByRole("button", { name: "5" });
 
       expect(allKeys).toBeInTheDocument();
+    });
+    test("Then it should show a button with the text 'delete' on it", () => {
+      render(<Keyboard />);
+
+      const deleteKey = screen.getByRole("button", { name: "delete" });
+
+      expect(deleteKey).toBeInTheDocument();
     });
   });
 });
